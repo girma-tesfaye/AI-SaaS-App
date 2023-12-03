f01a933e122afa643c6af92245e2ad84e3d6483a
girma-tesfaye <girmatesfaye80@gmail.com>
2023-12-03 00:15:22 -0800

    Create sidebar header dashboard and conversation AI UI

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,8 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  //publicRoutes: ["/"]
+  publicRoutes: ["/"],
+  //ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/dashboard"]
 });
  
 export const config = {

f7141e17c981258750996e1fc703ef07cf3021e3
girma-tesfaye <girmatesfaye80@gmail.com>
2023-12-13 12:37:12 -0800

    Fixed Update UI changes error

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -11,7 +11,7 @@ export const LandingHero = () => {
     return (
         <div className="text-white font-bold py-36 text-center space-y-5">
             <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-                <h1>Explore the AI's power</h1>
+                <h1>Explore the AI&apos;s power</h1>
                 <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
                     <TypewriterComponent 
                         options={{

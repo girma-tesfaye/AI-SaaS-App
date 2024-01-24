3ed0b172577d0546d734aff2282e5012336f84e7
girma-tesfaye <girmatesfaye80@gmail.com>
2024-01-24 14:21:24 -0800

    Update navbar content

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -30,7 +30,7 @@ export const LandingNavbar = () => {
             <div className="flex items-center gap-x-2">
                 <Link href={isSignedIn ? "/dashboard": "/sign-up"}>
                     <Button variant="outline" className="rounded-fill">
-                        Get Started
+                        Start exploring
                     </Button>
                 </Link>
             </div>
